8d2c1fe93252b13b799ca9dd5b2962e5e1c986ef
Aleksandr Vansach <Aleksandr.Vansach@eurochem.ru>
2019-11-13 15:05:51 +0700

    Fix algorithm for getting a die roll

--- src/Dice.js
+++ src/Dice.js
@@ -24,5 +24,5 @@ export default function Dice(props) {
 }
 
 export function getRollResult(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * max) + min;
 }
