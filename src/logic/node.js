7b67a479d5be0f17f138abba83c8146186ff44d2
Aleksandr Vansach <Aleksandr.Vansach@eurochem.ru>
2019-11-28 23:40:56 +0300

    Add "Node" class for graph usage

---
+++ src/logic/node.js
@@ -0,0 +1,46 @@
+export default class Node {
+  constructor(id) {
+    if (id) this.id = id;
+
+    this.connections = new Set();
+  }
+
+  /**
+   * Add connections between this and other node
+   *
+   * @param {Node} node
+   * @returns {Node}
+   */
+  linkWith(node) {
+    this.connections.add(node);
+    node.connections.add(this);
+
+    return this;
+  }
+
+  /**
+   * Remove connections between this and other node
+   *
+   * @param {Node} node
+   * @returns {Node}
+   */
+  unlinkFrom(node) {
+    this.connections.delete(node);
+    node.connections.delete(this);
+  }
+
+  /**
+   * Replaces all connections to this node with connections to provided target
+   *
+   * @param {Node} target
+   * @returns {Node}
+   */
+  mergeInto(target) {
+    for (const node of this.connections) {
+      this.unlinkFrom(node);
+      target.linkWith(node);
+    }
+
+    return target;
+  }
+}
---
+++ src/logic/node.test.js
@@ -0,0 +1,50 @@
+import Node from "./node";
+
+it("retains index", () => {
+  expect(new Node(1).id).toEqual(1);
+});
+
+it("connects to other node", () => {
+  const a = new Node();
+  const b = new Node();
+
+  a.linkWith(b);
+
+  expect(a.connections).toContain(b);
+  expect(b.connections).toContain(a);
+});
+
+it("replaces all connections on merge", () => {
+  const a = new Node();
+  const b = new Node();
+  const c = new Node();
+
+  a.linkWith(b);
+  a.mergeInto(c);
+
+  expect(a.connections).not.toContain(b);
+  expect(b.connections).not.toContain(a);
+
+  expect(b.connections).toContain(c);
+  expect(c.connections).toContain(b);
+});
+
+it("handles complex connections replacement", () => {
+  const a1 = new Node();
+  const a2 = new Node();
+  const b1 = new Node();
+  const b2 = new Node();
+
+  // a1-a2 b1-b2
+  a1.linkWith(a2);
+  b1.linkWith(b2);
+
+  // a1-a2-b2
+  b1.mergeInto(a2);
+
+  expect(b2.connections).not.toContain(b1);
+  expect(b1.connections).not.toContain(b2);
+
+  expect(a2.connections).toContain(b2);
+  expect(b2.connections).toContain(a2);
+});
