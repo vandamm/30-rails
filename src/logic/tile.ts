1b45043105a47699c9234ab4013d07339b7a00d8
Aleksandr Vansach <Aleksandr.Vansach@eurochem.ru>
2019-12-13 18:45:56 +0300

    Annotate tile.ts

--- src/logic/tile.ts
+++ src/logic/tile.ts
@@ -1,25 +1,40 @@
 import Node from "./node";
 import { Matrix, transform } from "./matrix";
 
+export type TileType = "TILE" | "MINE" | "STATION" | "MOUNTAIN";
+export interface Tile {
+  value: number;
+  type: TileType;
+  rotation: number;
+  flip?: boolean;
+  x: number;
+  y: number;
+}
+type NodeIndexPair = [number, number];
+interface TileConfig {
+  maxRotation: number;
+  connections: NodeIndexPair[];
+  canFlip?: boolean;
+}
+
 const MATRIX: Matrix = [
   [0, 1, 0],
   [2, 0, 3],
   [0, 4, 0]
 ];
-const SIZE = 3;
-const TILES = {
+const TILES: { [key: string]: TileConfig } = {
   // ⌜
-  1: {
+  "1": {
     maxRotation: 270,
     connections: [[3, 4]]
   },
   // |
-  2: {
+  "2": {
     maxRotation: 90,
     connections: [[1, 4]]
   },
   // ⌟⌜
-  3: {
+  "3": {
     maxRotation: 90,
     connections: [
       [1, 2],
@@ -27,7 +42,7 @@ const TILES = {
     ]
   },
   // +
-  4: {
+  "4": {
     maxRotation: 0,
     connections: [
       [1, 4],
@@ -35,7 +50,7 @@ const TILES = {
     ]
   },
   // ⌝⌜
-  5: {
+  "5": {
     maxRotation: 270,
     connections: [
       [2, 4],
@@ -43,7 +58,7 @@ const TILES = {
     ]
   },
   // ⌝|
-  6: {
+  "6": {
     maxRotation: 270,
     canFlip: true,
     connections: [
@@ -53,52 +68,47 @@ const TILES = {
   }
 };
 
-const TILE_TYPES = {
-  TILE: {
-    matrix: ({ value }) => connect(TILES[value].connections)
-  },
-  STATION: {
-    matrix({ value, x, y }) {
-      const node = new Node("STATION", value);
-      const matrix = [new Array(3), new Array(3), new Array(3)];
+const TILE_MATRICES: { [key in TileType]?: (tile: Tile) => Matrix } = {
+  TILE: tile => connect(TILES[tile.value].connections),
+  STATION: tile => {
+    const { value, x, y } = tile;
+    const node = new Node("STATION", value);
+    const matrix = [new Array(3), new Array(3), new Array(3)];
 
-      if (x === 0) matrix[2][1] = node;
-      else if (y === 0) matrix[1][2] = node;
-      else if (x > y) matrix[0][1] = node;
-      else matrix[1][0] = node;
+    if (x === 0) matrix[2][1] = node;
+    else if (y === 0) matrix[1][2] = node;
+    else if (x > y) matrix[0][1] = node;
+    else matrix[1][0] = node;
 
-      return matrix;
-    }
+    return matrix;
   },
-  MINE: {
-    matrix() {
-      return [
-        [null, new Node("MINE", 1), null],
-        [new Node("MINE", 2), null, new Node("MINE", 3)],
-        [null, new Node("MINE", 4), null]
-      ];
-    }
+  MINE: () => {
+    return [
+      [null, new Node("MINE", 1), null],
+      [new Node("MINE", 2), null, new Node("MINE", 3)],
+      [null, new Node("MINE", 4), null]
+    ];
   }
 };
 
-export function maxRotation(tile) {
+export function maxRotation(tile: string): number {
   return TILES[tile].maxRotation;
 }
 
-export function canFlip(tile) {
+export function canFlip(tile: string): boolean {
   return TILES[tile].canFlip || false;
 }
 
-export function getNodes(tile) {
+export function getNodes(tile: Tile) {
   const { type, rotation, flip } = tile;
-  const config = TILE_TYPES[type];
+  const matrix = TILE_MATRICES[type];
 
-  if (!config) return;
+  if (!matrix) return;
 
-  return transform({ rotation, flip }, config.matrix(tile));
+  return transform({ rotation, flip }, matrix(tile));
 }
 
-function connect(connections) {
+function connect(connections: NodeIndexPair[]): Matrix {
   const nodes = [0, 1, 2, 3, 4].map(() => new Node("TILE"));
 
   for (const [left, right] of connections) nodes[left].linkWith(nodes[right]);
