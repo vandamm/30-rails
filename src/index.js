08526be3d12283d1cf8e3109e2b12803340853e9
Alexander Vansach <vandamm@users.noreply.github.com>
2025-03-04 11:42:17 +0100

    Migrate project from react-scripts to vite
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/vandamm/30-rails?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import "./index.css";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const root = createRoot(rootElement);
+root.render(<App />);
